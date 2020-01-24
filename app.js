6a716f8869c8a1e41890796a1dbfe625cdceb4cb
Kalud.Z <khaled.fhm@live.fr>
2020-01-24 20:28:22 +0100

    updating UI implemented.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // MODULE PATTERN , uses IIFE and Closures #####################
 
 
-
-// Budget ##############################################################################################
+// #########################################################################################################################
+// BUDGET CONTROLLER ########################################################################################################
 var budgetController = (function() {
 
     var Expense = function(id, description, value) {
@@ -25,8 +25,19 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
+
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        // data.allItems[type] = sum;   this is the bug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        data.totals[type] = sum;
+    }
 
     return {
         addItem: function(type, des, val) {
@@ -41,7 +52,24 @@ var budgetController = (function() {
             data.allItems[type].push(newItem);      // push it into our data structure
             return newItem;         // so whoever called this method , gets direct access to this new created object.
         },
-        test: function(){
+
+        calculateBudget: function() {
+            calculateTotal('inc');
+            calculateTotal('exp');
+            data.budget = data.totals.inc - data.totals.exp;
+            data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc : data.totals.inc,
+                totalExp : data.totals.exp,
+                percentage: data.percentage
+            }
+        },
+
+        test: function(){  // just for testing purposes , u can delete it
             console.log(data);
         }
     }
@@ -49,7 +77,8 @@ var budgetController = (function() {
 })();
 
 
-// UI ###################################################################################################
+// ######################################################################################################################
+// UI ####################################################################################################################
 var UIController = (function() {
 
     var DOMStrings = {
@@ -58,8 +87,11 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
-
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     }
 
     return {
@@ -67,12 +99,37 @@ var UIController = (function() {
             return {
                 type:           document.querySelector(DOMStrings.inputType).value,  // => inc or exp
                 description:    document.querySelector(DOMStrings.inputDescription).value,
-                value:          document.querySelector(DOMStrings.inputValue).value 
+                value:          parseFloat(document.querySelector(DOMStrings.inputValue).value)
             }
+        },
+
+        clearFields: function(){
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ',' + DOMStrings.inputValue); // this returs a list. we wanna transform it into an array.
+            fieldsArr = Array.prototype.slice.call(fields);  // turn the list into an array | because is a method of array object. | all method of the array object , are in the Array.prototype | slice() : return a copy of the array.
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = '';
+            });  
+            fieldsArr[0].focus();        // return the cursor focused on description
+
         },
 
         getDOMStrings: function() {
             return DOMStrings;
+        },
+
+        displayBudget: function(obj) {
+            document.querySelector(DOMStrings.budgetLabel).textContent = obj.budget + '€';
+            document.querySelector(DOMStrings.incomeLabel).textContent = obj.totalInc + '€';
+            document.querySelector(DOMStrings.expensesLabel).textContent = obj.totalExp + '€';
+
+            /* if(obj.percentage <= 0) { document.querySelector(DOMStrings.percentageLabel).textContent = '---'; }
+            else { document.querySelector(DOMStrings.percentageLabel).textContent = obj.percentage + '%'; }
+ */
+
+            if(obj.percentage == Infinity) { document.querySelector(DOMStrings.percentageLabel).textContent = '---'; }
+            else { document.querySelector(DOMStrings.percentageLabel).textContent = obj.percentage + '%'; }
+
         },
 
         addListItem: function(obj, type) {
@@ -91,7 +148,7 @@ var UIController = (function() {
             //replace the place holder text with actual data
             newHtml = html.replace('%id%' , obj.id);
             newHtml = newHtml.replace('%description%' , obj.description);
-            newHtml = newHtml.replace('%value%' , obj.value);
+            newHtml = newHtml.replace('%value%' , obj.value + '€');
 
             //insert HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend' , newHtml);  // inserted as a last child of the element. 
@@ -100,8 +157,8 @@ var UIController = (function() {
 
 })();
 
-
-// Controller #############################################################################################
+// ######################################################################################################################
+//  GLOBAL APP CONTROLLER ###############################################################################################
 var controller = (function(budgetCtrl , UICtrl) {
     
     var setupEventListeners = function() {
@@ -110,26 +167,37 @@ var controller = (function(budgetCtrl , UICtrl) {
         document.addEventListener('keypress', function(event){ if(event.keyCode == 13) { ctrlAddItem(); }  });
     }
 
+
+    var updateBudget = function() {
+        // calculate the budget and dislay it on the UI
+        budgetCtrl.calculateBudget();
+        var budget = budgetCtrl.getBudget();
+        // console.log(budget);
+        UICtrl.displayBudget(budget);
+    }
+
     var ctrlAddItem = function() {  // this method is called only upon the click event.
         var input, newItem;
         input = UICtrl.getInput();   // extract the input added by the user
 
-        // add the item the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-        // add the item to the UI 
+        if(input.description !== '' && !isNaN(input.value) && input.value > 0) {
+            // add the item the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            // add the item to the UI 
+            UICtrl.addListItem(newItem, input.type);
+            // clear field
+            UICtrl.clearFields();
 
-        // calculate the budget and dislay it on the UI
+            updateBudget(); 
+        }
 
-        budgetCtrl.test();
-
-        UICtrl.addListItem(newItem, input.type);
     } 
 
     return {
         init: function() {
             console.log('the app started');
             setupEventListeners();
-           
+
         }
     }
 
